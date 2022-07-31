2af706f2a9004e39beff4ec40e76e9152ad7101a
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-07-31 20:40:21 +0100

    api

--- controllers/order.js
+++ controllers/order.js
@@ -15,17 +15,35 @@ exports.takeOrder = async (req, res) => {
 
     try {
         const order = await Order.findOne({origin})
-        return res.status(StatusCodes.OK).json({status : success, order})
+        return res.status(StatusCodes.OK).json({status : 'Taken', order})
     } catch (error) {
-        return res.status(StatusCodes.BAD_REQUEST).json({error : 'Taken!'})
+        return res.status(StatusCodes.BAD_REQUEST).json({error})
     }
 }
 
 exports.orderList = async (req, res) => {
+    const {origin, destination} = req.query
+    const queryObject = {}
+
+    if (origin) {
+        queryObject.origin = origin
+    }
+
+    if (destination) {
+        queryObject.destination = origin
+    }
+
+    let result = Order.find(queryObject)
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+    result = result.skip(skip).limit(limit)
+
     try {
-        const orders = await Order.find()
-        return res.status(StatusCodes.OK).json({orders, cont : orders.lenght})
+        const queries = await result
+        const orders = await Order.find(queries)
+        return res.status(StatusCodes.OK).json({orders, count : orders.lenght})
     } catch (error) {
-        return res.status(StatusCodes.NOT_FOUND).json({error : `Order not found!`})
+        return res.status(StatusCodes.NOT_FOUND).json(error) 
     }
 }
