813fd9c9a819e2ad570f2a4d795ea62ee745438d
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-07-31 15:09:41 +0100

    take order

--- controllers/order.js
+++ controllers/order.js
@@ -1,12 +1,31 @@
+const { StatusCodes } = require("http-status-codes")
+const Order = require("../models/order")
 
 exports.placeOrder = async (req, res) => {
-    const {origin, destination} = req.body
+    try {
+        const newOrder = await Order.create({...req.body})
+        return res.status(StatusCodes.CREATED).json({newOrder})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error : 'There was an error!'})
+    }
 }
 
 exports.takeOrder = async (req, res) => {
+    const {origin} = req.body
 
+    try {
+        const order = await Order.findOne({origin})
+        return res.status(StatusCodes.OK).json({status : success, order})
+    } catch (error) {
+        return res.status(StatusCodes.BAD_REQUEST).json({error : 'Order has been taken!'})
+    }
 }
 
 exports.orderList = async (req, res) => {
-    
+    try {
+        const orders = await Order.find()
+        return res.status(StatusCodes.OK).json({orders, cont : orders.lenght})
+    } catch (error) {
+        return res.status(StatusCodes.NOT_FOUND).json({error : `Order not found!`})
+    }
 }
