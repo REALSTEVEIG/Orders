ef7494ba8d783c4b97bc4c8ccbce6253ac1fe76c
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-07-31 21:10:01 +0100

    production

--- app.js
+++ app.js
@@ -8,9 +8,24 @@ const connectDB = require('./db/connect')
 const notfoundMiddleware = require('./middleware/notfound')
 const errorHandlerMiddleware = require('./middleware/errorhandler')
 const orderRouter = require('./routes/order')
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 
 app.use('/', orderRouter)
